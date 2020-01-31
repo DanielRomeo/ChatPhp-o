881f76cfca24de362a8177579855d53bf2bdc566
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-01-31 22:01:05 +0200

    Design edit on the Users.php primarily

--- user.php
+++ user.php
@@ -13,7 +13,7 @@
 
 	session_start();
 
-	echo "you are logged in as ". $_SESSION['username'];
+	//echo "you are logged in as ". $_SESSION['username'];
 
 
 	// Make sure the _GET username is set, and sanitize it
@@ -38,8 +38,10 @@
 	// get the name and info of the page owner:
 	$sql = "SELECT * FROM users WHERE username='$username'  LIMIT 1";
 	$query = mysqli_query($db_conx, $sql);
-	$profileName = mysqli_fetch_row($query);
-	$profileName = $profileName[1];
+	$query = mysqli_fetch_row($query);
+	$profileName = $query[1];
+	$firstName = $query[2];
+	$lastName = $query[3];
 
 ?>
 
@@ -52,40 +54,72 @@
 </head>
 <body>
 
-	<!-- <h3>hello</h3>
-
-	<h4>Page of </h4>
-	<a href="logout.php" class="btn btn-default">Logout</a> -->
-
-	<!-- Image and text -->
+	<!-- start of the navigation -->
 	<nav class="navbar navbar-light bg-light">
 	  <a class="navbar-brand" href="#">
 	    <img src="images/macbaselogo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-	    Bootstrap
+	    @<?php echo $username; ?>
 	  </a>
 	  <li class="nav-item active">
         <a class="nav-link" href="#">Logout <span class="sr-only"></span></a>
       </li>
 	</nav>
+	<!-- end of the navigation -->
 
-	<div id="mainSection">
+	<div id="mainSection" class="container">
 
+		<div class="jumbotron">
 
+			<div class="mainContainer" class="container">
 
-		<div id="mainContainer" class="container">
+				<div class="row">
+
+					<div class="col-lg-4 col-md-8 col-sm-12">
+						<img class="mb-4" src="images/macbaselogo.png" class="img-thumbnail" width="130" height="130">
+					</div>
+
+					<div class="col-lg-8 col-md-8 col-sm-12">
+						<h4 class="display-4"><?php echo strtoupper($firstName).' '.strtoupper($lastName) ?></h4>
+
+						<!-- here goes the profile button: -->
+						<a id="profileButton" href="" class="btn btn-primary">Edit Profile</a>
+
+						<br /> <br>
+
+						<!-- here goes the bio -->
+						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div> <!-- end of row-->
+
+				
+
+				<hr class="my-4">
+				
+				<!-- statuses, friends and last login -->
+				<div class="row">
+					<div class="col-md-4">
+						hello
+						<!-- number of statuses -->
+					</div>	
+					<div class="col-md-4">
+						hello
+						<!-- number of friends -->
+					</div>	
+					<div class="col-md-4">
+						hello
+						<!-- last login date: -->
+					</div>	
+				</div>
+
+			</div> <!-- end of mainContainer-->
+		</div><!-- end of jumbotron-->
+
 		
-			<div class="row">
-
-				<div class="col-lg-4">
-					<img class="mb-4" src="images/macbaselogo.png" class="img-thumbnail" width="130" height="130">
-				</div>
-
-				<div>
-					<h4><?php echo strtoupper($profileName) ?></h4>
-				</div>
-			</div>
-
-		</div>
 	</div>
 
 	
